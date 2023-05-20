3e530f246496f3b84b1e301d7efe6c88bef9943b
schetilt <shetilin.timofey@gmail.com>
2023-05-20 15:59:27 +0200

    Implemented diagonalization of form matrices 1/2

---
+++ src/quadratic_forms.py
@@ -0,0 +1,41 @@
+#!/bin/usr/env python3
+
+import numpy as np
+
+"""
+    Gets matrix of quadratic form and returns its diagonalization: 
+    diagonal matrix D and transition matrix Q. Then A = Q^{T} A Q
+
+    A should be a symetric matrix.
+"""
+def diagonalize_form(A):
+    n, m = np.array(A).shape
+    if n != m:
+        raise ValueError("A should be a square matrix")
+
+    identity_matrix = np.identity(n)
+    
+    for k in range(n):
+        potential_pivots  = [abs(A[i][k]) for i in range(n)]
+        if max(potential_pivots) != A[k][k]:
+            i_max = potential_pivots.index(max(potential_pivots)) + k
+        
+            A[k], A[i_max] = A[i_max], A[k]
+            A = np.transpose(np.array(A)).tolist()
+            A[k], A[i_max] = A[i_max], A[k]
+
+
+        A = np.transpose(np.array(A)).tolist()
+        
+        for i in range(k+1, n):
+            c = A[i][k] / A[k][k]
+            for j in range(k+1, n):
+                A[i][j] = round(A[i][j] - c * A[k][j],2)
+                
+            A[i][k] = 0
+            for j in range(k+1, n):
+                A[j][i] = round(A[j][i] - c * A[j][k], 2)
+            
+            A[k][i] = 0 
+    
+    return (A, identity_matrix)
