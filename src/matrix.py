0dae99a4daf647f02d869e0d0b86a60a45910433
schetilt <shetilin.timofey@gmail.com>
2023-05-28 23:04:02 +0200

    Bug fixed in Matrix and added more tests

--- src/matrix.py
+++ src/matrix.py
@@ -7,22 +7,29 @@ class Matrix:
         if (self.rows <= 0 or self.col <= 0):
             raise ValueError("Dimensions of the matrix should be greater than 0.");
         
-
+    @staticmethod
+    def create_zero_matrix(rows_number, cols_number):
+        arr = []
+        for i in range(rows_number):
+            arr.append(cols_number*[0])
+        return Matrix(arr)
 
     def __eq__(self, other):
         return self.matrix == other.matrix
 
     def __sub__(self, other):
+        if (type(other) != Matrix):
+            raise TypeError(f"You can add only Matrix and Matrix. Given argument is {type(other)} type.")
         if (self.rows != other.rows or self.col != other.col):
             raise ValueError("Matrices should be of the same size to perform subtraction.")
     
-        res = [0 for i in range(self.rows)] if (self.col == 1) else [self.rows*[0 for i in range(self.col)]]
+        res = []
+        for k in range(self.rows):
+            res.append([0 for i in range(self.col)])
+        
         for i in range(self.rows):
-            if (self.col != 1):
-                for j in range(self.col):
-                    res[i][j] = self.matrix[i][j] + other.matrix[i][j]
-            else:
-                res[i] = self.matrix[i] - other.matrix[i] 
+            for j in range(self.col):
+                res[i][j] = self.matrix[i][j] - other.matrix[i][j]
 
         return Matrix(res)
 
@@ -30,7 +37,7 @@ class Matrix:
         if (type(other) != Matrix):
             raise TypeError(f"You can add only Matrix and Matrix. Given argument is {type(other)} type.")
         if (self.rows != other.rows or self.col != other.col):
-            raise ValueError("Matrices should be of the same size to perform subtraction.")
+            raise ValueError("Matrices should be of the same size to perform addition.")
     
         res = []
         for k in range(self.rows):
@@ -43,19 +50,30 @@ class Matrix:
         return Matrix(res)
     
     def __mul__(self, a):
-        result = self.matrix.copy()
+        result = None
         # a is a scalar
         if type(a) == int or type(a) == float:
+            result = self.matrix.copy()
             for i in range(self.rows):
                 for j in range(self.col):
                     if (self.col != 1):
                         result[i][j] *= a
                     else: 
                         result[i] *= a
-        # dot product: only for vectors
-        else:
-        #TODO
-            pass
+        # a is an another matrix
+        elif type(a)==Matrix:
+            if (type(a) != Matrix):
+                raise TypeError(f"You can add only Matrix and Matrix. Given argument is {type(other)} type.")
+            if (self.rows != a.rows or self.col != a.col):
+                raise ValueError("Matrices should be of the same size to perform addition.")
+
+            result = Matrix.create_zero_matrix(self.rows, self.col).matrix
+            for i in range(self.rows):
+                for j in range(a.col):
+                    element = 0
+                    for k in range(self.col):
+                        element += self.matrix[i][k]*a.matrix[k][j]
+                    result[i][j] = element
         return Matrix(result)
 
     def __truediv__(self, a):
--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,16 +1,37 @@
+#!/bin/usr/env python3
+
 import sys
 sys.path.append('../linra')
 
 from src.matrix import Matrix
 import unittest
 
-class TestMatrix(unittest.TestCase):
+class TestData:
+    a = Matrix([[1, 2, 3], [10, -12, 7], [0, 2, -4]])
+    b = Matrix([[5, 9, 7], [-8, 4, 9], [10, 4, 3]])
+    c = Matrix([[2,2], [4,1]])
 
+class TestMatrix(unittest.TestCase):
     def test_add(self):
-        a = Matrix([[1, 2, 3], [10, -12, 7], [0, 2, -4]])
-        b = Matrix([[5, 9, 7], [-8, 4, 9], [10, 4, 3]])
         res = Matrix([[6, 11, 10], [2, -8, 16], [10, 6, -1]])
-        self.assertEqual((a+b), res)
+        self.assertEqual((TestData.a+TestData.b), res)
+
+        with self.assertRaises(ValueError):
+            TestData.a+TestData.c
+    
+    def test_sub(self):
+        res = Matrix([[-4, -7, -4], [18, -16, -2], [-10, -2, -7]])
+        self.assertEqual((TestData.a-TestData.b), res)
+        
+        with self.assertRaises(ValueError):
+            TestData.a-TestData.c
+
+    def test_mul(self):
+        res = Matrix([[19, 29, 34], [216, 70, -17], [-56, -8, 6]])
+        self.assertEqual((TestData.a*TestData.b), res)
+
+        with self.assertRaises(ValueError):
+            TestData.a*TestData.c
 
 if __name__ == "__main__":
     unittest.main()
