b8e0a88d1c4536050ea93f0e1591ace128b7d329
schetilt <shetilin.timofey@gmail.com>
2023-06-01 15:04:42 +0200

    Bug fixes in Vector

--- src/vector.py
+++ src/vector.py
@@ -7,6 +7,8 @@ class Vector(Matrix):
         self.cols = 1
     
     def __add__(self, other):
+        self._validate_type(other)
+        self._validate_size(other)
         res = list(map(lambda x, y: x+y, self._data, other._data))
         return Vector(res)
     
@@ -14,8 +16,18 @@ class Vector(Matrix):
         return self.__add__(self, other)
 
     def __sub__(self, other):
+        self._validate_type(other)
+        self._validate_size(other)
         res = list(map(lambda x, y: x-y, self._data, other._data))
         return Vector(res)
     
     def __rsub__(other, self):
         return self.__sub__(other, self)
+    
+    def _validate_type(self, other):
+        if type(other) != Vector:
+            raise TypeError(f"You can perform this operation only with a Vector. {other} is not of type Vector.")
+        
+    def _validate_size(self, other):
+        if len(self._data) != len(other._data):
+            raise ValueError(f"Given vectors are not of the same size.")
