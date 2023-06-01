ed2b711735ac784df81394dd77e66bf10ad59595
schetilt <shetilin.timofey@gmail.com>
2023-06-01 23:44:25 +0200

    Extended Vector class

--- src/vector.py
+++ src/vector.py
@@ -24,9 +24,29 @@ class Vector(Matrix):
     def __rsub__(other, self):
         return self.__sub__(other, self)
     
+    def __truediv__(self, a):
+        if type(a) != int and type(a) != float:
+            raise TypeError(f"You can't divide a vector by anything except int and float. The given object is a {type(a)} type.")
+        
+        res = self._data
+        for i in range(len(self._data)):
+            res[i] /= a
+
+        return Vector(res)
+    
+    def __mul__(self, other):
+        if type(other) != int and type(other) != float:
+            raise TypeError(f"You can't divide a vector by anything except int and float. The given object is a {type(a)} type.")
+        
+        res = self._data
+        for i in range(len(self._data)):
+            res[i] *= other
+
+        return Vector(res)
+
     def _validate_type(self, other):
         if type(other) != Vector:
-            raise TypeError(f"You can perform this operation only with a Vector. {other} is not of type Vector.")
+            raise TypeError(f"You can perform this operation only with a Vector. {other} is not of type Vector. It is {type(other)}.")
         
     def _validate_size(self, other):
         if len(self._data) != len(other._data):
